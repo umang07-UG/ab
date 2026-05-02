2f7481f005e58fcb95cf9b47a9ddef98a3fdf1fb
Godhaniya Umang <godhaniyaumang007@gmail.com>
2026-05-02 14:02:29 +0530

    network

--- myproject/settings.py
+++ myproject/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-change-me-in-producti
 
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost', cast=Csv())
+# ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost', cast=Csv())
 
 INSTALLED_APPS = [
     'myapp',
