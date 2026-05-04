42ea51e42abef054dbdfb85db3f65747d52a3194
umang07-UG <umana007@gmail.com>
2026-05-05 00:35:47 +0530

    Add admin authentication with email admin1@gmail.com password 12 and error handling

---
+++ myenv/myproject/myapp/management/commands/create_admin.py
@@ -0,0 +1,21 @@
+from django.core.management.base import BaseCommand
+from myapp.models import User
+
+class Command(BaseCommand):
+    help = 'Create admin user if not exists'
+
+    def handle(self, *args, **kwargs):
+        admin_email = 'admin1@gmail.com'
+        admin_password = '12'
+        admin_name = 'Admin User'
+
+        if not User.objects.filter(email=admin_email).exists():
+            User.objects.create(
+                name=admin_name,
+                email=admin_email,
+                password=admin_password,
+                mobile=0
+            )
+            self.stdout.write(self.style.SUCCESS(f'Admin user created: {admin_email}'))
+        else:
+            self.stdout.write(self.style.WARNING(f'Admin user already exists: {admin_email}'))
