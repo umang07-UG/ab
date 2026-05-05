ec3d07a907b2263f23cde61af7b56f4029c29068
umang07-UG <umana007@gmail.com>
2026-05-05 21:21:40 +0530

    change admin url from /admin/ to /avd/

--- myproject/urls.py
+++ myproject/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 from django.views.generic import RedirectView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('avd/', admin.site.urls),
     path('favicon.ico', RedirectView.as_view(url='/static/favicon.ico', permanent=True)),
     path('', include('myapp.urls'))
 ]
