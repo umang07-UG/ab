bc9b1448f8d9c19e493180770e171c5d83775260
umang07-UG <umana007@gmail.com>
2026-05-08 22:09:08 +0530

    Fix: Use CompressedStaticFilesStorage to fix missing manifest error on Railway

--- myproject/settings.py
+++ myproject/settings.py
@@ -74,7 +74,7 @@ STORAGES = {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
